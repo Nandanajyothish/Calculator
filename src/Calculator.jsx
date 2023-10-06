47921990dd72c3d00737e8952b89509ba5037e23
don dep <dondep613@gmail.com>
2023-10-06 20:05:09 +0530

    first commit

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -12,6 +12,9 @@ const  clearInput=()=>{
     setInput('');
     setResult('');
 }
+const handleDelete=()=>{
+    setInput(input.slice(0,-1))
+}
 const calculateResult=()=>{
     try{
     setInput(eval(input).toString())
@@ -19,6 +22,8 @@ const calculateResult=()=>{
     catch(error){
         setResult('Error')
     }
+    
+   
 }
 
 
@@ -28,32 +33,35 @@ const calculateResult=()=>{
         <div className='input'>{input}{result}</div>
        
         <div className='buttons'>
-            
+        <button onClick={clearInput}>C</button> 
+        <button onClick={()=>handleinput('%')}>%</button>
+        <button onClick={handleDelete}>Del</button>
+        <button  onClick={()=>handleinput('/')}>/</button>
          <button onClick={()=>handleinput('7')}>7</button> 
          <button onClick={()=>handleinput('8')}>8</button>  
-         <button onClick={()=>handleinput('9')}>9</button>  
-         <button onClick={()=>handleinput('+')}>+</button>  
+         <button onClick={()=>handleinput('9')}>9</button> 
+         <button onClick={()=>handleinput('*')}>*</button>   
          <button onClick={()=>handleinput('4')}>4</button>  
          <button onClick={()=>handleinput('5')}>5</button>  
          <button onClick={()=>handleinput('6')}>6</button>  
          <button onClick={()=>handleinput('-')}>-</button>  
          <button onClick={()=>handleinput('1')}>1</button>  
          <button onClick={()=>handleinput('2')}>2</button> 
-         <button onClick={()=>handleinput('3')}>3</button>  
-         <button onClick={()=>handleinput('*')}>*</button>
-         <button onClick={()=>handleinput('00')}>00</button> 
-         <button onClick={()=>handleinput('0')}>0</button>  
-         <button onClick={()=>handleinput('.')}>.</button> 
-         <button onClick={()=>handleinput('/')}>/</button>
-         <button onClick={()=>handleinput('%')}>%</button>
-         <button onClick={clearInput}>C</button> 
+         <button onClick={()=>handleinput('3')}>3</button>
+         <button onClick={()=>handleinput('+')}>+</button>    
         
-         <button onClick={calculateResult}>=</button>
+         <button onClick={()=>handleinput('00')}>0</button> 
+         <button onClick={()=>handleinput('0')}>00</button>  
+         <button onClick={()=>handleinput('.')}>.</button> 
          
-
+         
+         
+        <button  onClick={calculateResult}>=</button>
+         
+           
          
         </div>
-      
+        
     </div>
   );
 }
